8c21bfb9c9ea85f63d155063dfa9adcbb023dec3
Hanada <im@hanada.info>
2020-06-18 11:57:48 +0800

    Initial commit

---
+++ config.js
@@ -0,0 +1,25 @@
+window.Config = {
+
+  SiteName: 'Tamers Union Server Status',
+
+  SiteUrl: '/',
+
+  ApiKeys: [
+    'ur875210-c9394842b4e834e00806c786',
+  ],
+
+  ShowLink: false,
+
+  CountDays: 60,
+
+  Navi: [
+    {
+      text: 'Home',
+      url: 'https://www.tamersunion.net/'
+    },
+    {
+      text: 'Docs',
+      url: 'https://docs.tamersunion.net/'
+    }
+  ]
+};
